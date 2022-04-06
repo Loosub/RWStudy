faa26bb25163dace60a151b42819427d48439d8f
승현 표 <vytmd0204@gmail.com>
2022-04-07 00:53:30 +0900

    [PSH] 반응형웹 5주차 하노이의 탑

---
+++ Lecture05/5w/Hanoi.js
@@ -0,0 +1,8 @@
+function Init(){
+    document.getElementById('red').style.display = 'none';
+    document.getElementById('yellow').style.display = 'none';
+    document.getElementById('green').style.display = 'none';
+    document.getElementById('blue').style.display = 'none';
+    document.getElementById('purple').style.display = 'none';
+    document.getElementById('black').style.display = 'none';
+}
